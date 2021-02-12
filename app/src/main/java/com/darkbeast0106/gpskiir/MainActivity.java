ca366d88afcb4a526b04862a8d31891060f903a6
Budaházi Bence <budahazi.bence@petrik.hu>
2021-02-12 15:31:16 +0100

    Kód átszervezése, kommentelése.

--- app/src/main/java/com/darkbeast0106/gpskiir/MainActivity.java
+++ app/src/main/java/com/darkbeast0106/gpskiir/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -33,46 +34,92 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.gps_text);
-        writePermission = false;
+        init();
 
+        // Vizságlat, hogy van-e engedély a helyeadatok lekérdezéséhez.
+        // Android Studio alt+enter segítségével legenerálja, lásd lentebb.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            textView.setText(R.string.no_gps_permission);
+
+            // Engedély kérés ablak megnyitása.
+            String[] permissions =
+                new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            };
+            ActivityCompat.requestPermissions(this, permissions, 0);
+            return;
+        }
+        // LocationManagernél a Listener hozzáadásakor azt is meg kell adni,
+        // hogy mely szolgáltató adja a helyadatokat, illetve azt is,
+        // hogy milyen gyakran és mekkora táv megtétele után történjen esemény.
+        // 0, 0 megadásával folyamatos a helyadat frissítése.
+        // Kötelező előtte ellenőrizni, hogy meg van-e a szükséges engedély.
+        // Az ellenőrző blokk alt+enter-rel legenerálható.
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0, 0, locationListener);
+    }
+
+    private void init() {
+        textView = findViewById(R.id.gps_text);
+
+        //LocationManager
+        //Ez felelős a hely lekérdezésért.
         locationManager =
                 (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        //LocationListener:
+        //LocationManager eseménykezelője, megmondja, hogy mi történjen, amikor helyadatot kér le.
+        //Létrehozásakor lambdát alkalmazható:
         locationListener = location -> {
             longitude = location.getLongitude();
             latitude = location.getLatitude();
         };
+        //Lambda nélküli szintaxis:
+        /*
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+        };
+         */
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            textView.setText(R.string.no_gps_permission);
-            String[] permissions =
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-            ActivityCompat.requestPermissions(this, permissions, 0);
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                0, 0, locationListener);
+        // Vizsgálat, hogy van-e endegély fájlt olvasni és fájlba írni a telefon tárhelyén
+        // External Strorage ne tévesszen meg, ez vonatkozik a beépített tárhelyre is
+        // Azért számít külső tárhelynek, mert az alkalmazásnak is van külön belső tárhelye
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED ||
             ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
+                    != PackageManager.PERMISSION_GRANTED) {
+
+            writePermission = false;
+            // Engedély kérés ablak megnyitása.
             String[] permissions =
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE};
             ActivityCompat.requestPermissions(this, permissions, 1);
-            writePermission = false;
         }else{
             writePermission = true;
         }
     }
 
+    // onResume akkor hívódik meg, amikor az alkalmazás előtérbe kerül.
+    // Mehívódik az alkalmazás létrejöttekor is, illetve ha újra megnyitjuk a háttérből,
+    // vagy bezárjuk a rajta lévő felugró ablakokat.
+    // Timert legtöbb esetben itt érdemes létrehozni és onPauseban leállítani.
     @Override
     protected void onResume() {
+        // Javas Timer leállítás után megszűnik és nem lehet újra indítani,
+        // új példányt kell csinálni belőle.
         timer = new Timer();
+        // TimerTask az esemény amit a timer végez,
+        // csak egy Timerhez rendelhető hozzá, így új timer esetény új task is kell.
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
@@ -83,14 +130,22 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
     }
 
+    // Megmondja, hogy mi történjen miután a felhasználó megadta vagy
+    // megtagadta a különböző engedélyeket.
+    // A requestCode az engedélykéréskor átadott requestCode,
+    // a permissions tömb tartalmazza a kérésben átadott összes permissiont,
+    // a grantResults tartalmazza, hogy az előbbi tömbből az azonos indexű engedély meg lett-e adva.
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                   @NonNull int[] grantResults) {
         if (requestCode == 0) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED ||
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED)
             {
+                // Mivel a requestLocationUpdates nem hívható meg a fenti ellenőrző block nélkül
+                // Így itt nem használhatjuk a grantResults tömböt
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                         0, 0, locationListener);
             }
@@ -105,6 +160,9 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    // onPause akkor hívódik meg, amikor az alkalmazás már nincs az előtérben
+    // pl.: lerakjuk, vagy felugró ablak nyílik meg rajta.
+    // Ilyenkor érdemes lehet leállítani a timert, hogy a háttérben ne fusson tovább.
     @Override
     protected void onPause() {
         timer.cancel();
@@ -115,10 +173,13 @@ public class MainActivity extends AppCompatActivity {
         this.runOnUiThread(TimerTick);
     }
 
+    // A textViewot nem tudja külső szál módosítani, ezért,
+    // hogy Timerrel tudjuk módosítani a szöveget szükség van 1 Runnable típúsú objektumra.
     private final Runnable TimerTick = new Runnable(){
         @Override
         public void run() {
             //String locationText = String.format("Lat: %f\r\nLong:%f",latitude,longitude);
+            // getString alkmas formátum stringbe a változók behelyezésére is.
             String locationText = getString(R.string.location_format, latitude,longitude);
             textView.setText(locationText);
             if (writePermission){
--- app/src/main/java/com/darkbeast0106/gpskiir/Naplozas.java
+++ app/src/main/java/com/darkbeast0106/gpskiir/Naplozas.java
@@ -12,19 +12,29 @@ import java.util.Date;
 
 public class Naplozas {
 
+    //Fájlbaírás, IOExceptiont dobhat ám ezt a MainActivityn tervezzük lekezelni.
     public static void kiir(double longitude, double latitude) throws IOException {
+        //Dátum-idő lekérdezése
         Date datum = Calendar.getInstance().getTime();
 
+        // Dátum formázására használt objektum. A formázásokat lásd:
+        // https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String formazottDatum = df.format(datum);
 
+        //CSV fájl egy sora. Formátum: longitude,latitude,dátum
         String sor = String.format("%f,%f,%s",longitude,latitude,formazottDatum);
-        String state = Environment.getExternalStorageState();
 
+        //Megnézi a belső tárhely állapotát.
+        String state = Environment.getExternalStorageState();
         if (state.equals(Environment.MEDIA_MOUNTED)){
+            //Filebaírás FileWriter+Buffered writer segítségével, felülírás helyett hozzáfűzéssel
             File file = new File(Environment.getExternalStorageDirectory(),"gps_adatok.csv");
             BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
             bw.append(sor);
+            // Nincs olyan függvény ami alapból sortörést is tenne a sorok végére,
+            // Ezt nekünk kell megtenni. \r\n illetve sima \n helyett érdemesebb
+            // az operációs rendszer alap elválasztó karakterét odaírni.
             bw.append(System.lineSeparator());
             bw.close();
         }
